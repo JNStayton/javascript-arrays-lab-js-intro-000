04386d781dc741c546fd1c80ec6c6e14eb26696c
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-27 01:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var kittens = [] //define your array here
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garlield"
+] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
