1fd651b4baaa82e2e68aae8dd4f741eb4cad5c5b
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-27 01:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,15 +28,20 @@ function destructivelyRemoveFirstKitten() {
 
 
 function appendKitten(name) {
-  return [...kittens, name]
+  return [...kittens, name];
 }
 
 
 function prependKitten(name) {
-  return [name, ...kittens]
+  return [name, ...kittens];
 }
 
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
+}
+
+
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
